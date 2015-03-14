ba575f5944aa83291788e9e870146d3a0d2b5a81
jak <jakealbrecht@gmail.com>
2015-03-14 11:42:40 -0400

    add NVIDA drivers to script

--- setup.sh
+++ setup.sh
@@ -5,10 +5,13 @@
 # Install basics
 
 sudo apt-get install -y git
-sudo apt-get install -y curl
 sudo apt-get install -y python-pip
 sudo apt-get install -y python-dev
 sudo apt-get install -y ipython
+sudo apt-get install -y unzip
+sudo apt-get install -y zlibc zlib1g zlib1g-dev
+sudo apt-get install -y lynx
+sudo apt-get install -y libjpeg-dev
 
 # Install emacs24
 # https://launchpad.net/~cassou/+archive/emacs
@@ -22,15 +25,35 @@ ln -sf dotfiles/.emacs.d .
 
 git clone https://github.com/chepyle/datascience-bowl
 
+#http://tleyden.github.io/blog/2014/10/25/cuda-6-dot-5-on-aws-gpu-instance-running-ubuntu-14-dot-04/
+# update NVIDIA driver
+wget http://us.download.nvidia.com/XFree86/Linux-x86_64/346.47/NVIDIA-Linux-x86_64-346.47.run
+
+sudo apt-get install -y linux-image-extra-virtual
+
+echo options nouveau modeset=0 | sudo tee -a /etc/modprobe.d/nouveau-kms.conf
+
+sudo update-initramfs -u
+
+sudo apt-get install -y linux-source
+sudo apt-get install -y linux-headers-3.13.0-46-generic # use `uname -r` to get version
+
+sudo bash NVIDIA-Linux-x86_64-346.47.run
+
+#
+
 cd datascience-bowl
 
 sudo pip install -r requirements-gpu.pip
 
-wget https://www.kaggle.com/c/datasciencebowl/download/train.zip
-wget https://www.kaggle.com/c/datasciencebowl/download/test.zip
-unzip train.zip
-unzip test.zip
+# must get graph create key from dato.com
+
+# will have to get the data manually using lynx... remove these lines
+#wget https://www.kaggle.com/c/datasciencebowl/download/train.zip
+#wget https://www.kaggle.com/c/datasciencebowl/download/test.zip
+#unzip train.zip
+#unzip test.zip
 
 
 
-python make_submission.py
+#python make_submission.py
